71226c3a05fb709ca6e47ccfad49ff41022ac9e2
zhenglong <zhenglong@bilibili.com>
2020-03-21 17:53:40 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 set -e
+pwd=`pwd`
 pip install pip -U -i  https://pypi.douban.com/simple
 pip install -r requirements.txt -i  https://pypi.douban.com/simple
 eval sed 's/1234567890XXXXXX/$1/' Authenticator_T.py > Authenticator.py
-echo "python `pwd`/Authenticator.py &" >> ~/.bash_profile
+
+cnt=`grep "python $pwd/Authenticator.py &" ~/.bash_profile|wc -l`
+if [ 1 -ne $cnt ] ;then
+echo "python $pwd/Authenticator.py &" >> ~/.bash_profile
+fi
 source ~/.bash_profile
